a0109f7790a36aac632880858690a1399cb86234
Nestor De la Cruz <nestoredelacruz@gmail.com>
2017-03-13 23:29:16 -0300

    Configured Facebook config

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -19,11 +19,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |  facebook_auth_on_load         boolean  Set to TRUE to have the library to check for valid access token on every page load.
 */
 
-$config['facebook_app_id']              = '';
-$config['facebook_app_secret']          = '';
+$config['facebook_app_id']              = '1823829894500693';
+$config['facebook_app_secret']          = '02e6a6b268cc166d162a8c49d3197f17';
 $config['facebook_login_type']          = 'web';
-$config['facebook_login_redirect_url']  = 'example/web_login';
-$config['facebook_logout_redirect_url'] = 'example/logout';
+$config['facebook_login_redirect_url']  = 'admin/publicar_anuncio';
+$config['facebook_logout_redirect_url'] = 'admin/register';
 $config['facebook_permissions']         = array('public_profile', 'publish_actions', 'email');
 $config['facebook_graph_version']       = 'v2.6';
 $config['facebook_auth_on_load']        = TRUE;
