301a05783869c1b1406f91ff741dcf8e0db7b837
Nestor De la Cruz <nestoredelacruz@gmail.com>
2017-03-13 22:59:27 -0300

    Added the dafault_timezone to the Admin and Start controller to avoid the date not safe error

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,49 +1,50 @@
 <?php
+date_default_timezone_set('America/Santo_Domingo');
+
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Admin extends CI_Controller{
-
-  public function __construct()
-  {
-    parent::__construct();
-    //Codeigniter : Write Less Do More
-  }
-
-  function index()
-  {
-    //The first and third line imports the templates
-    // which are in the templates folder under views.
-    // the middle one is to render the view you want to see.
-    $this->load->view('templates/top');
-    $this->load->view('admin/start');
-    $this->load->view('templates/footer');
-  }
-function publicar_anuncio(){
-  //The first and third line imports the templates
-  // which are in the templates folder under views.
-  // the middle one is to render the view you want to see.
-  $this->load->view('templates/top');
-  $this->load->view("admin/publicar_anuncio");
-  $this->load->view('templates/footer');
-}
-
-function login(){
-  //The first and third line imports the templates
-  // which are in the templates folder under views.
-  // the middle one is to render the view you want to see.
-  $this->load->view('templates/top');
-  $this->load->view("admin/login");
-  $this->load->view('templates/footer');
-
-}
-function register(){
-  //The first and third line imports the templates
-  // which are in the templates folder under views.
-  // the middle one is to render the view you want to see.
-  $this->load->view('templates/top');
-  $this->load->view("admin/register");
-  $this->load->view('templates/footer');
-
-}
-
+    
+    public function __construct()
+    {
+        parent::__construct();
+        //Codeigniter : Write Less Do More
+    }
+    
+    function index()
+    {
+        //The first and third line imports the templates
+        // which are in the templates folder under views.
+        // the middle one is to render the view you want to see.
+        $this->load->view('templates/top');
+        $this->load->view('admin/start');
+        $this->load->view('templates/footer');
+    }
+    function publicar_anuncio(){
+        //The first and third line imports the templates
+        // which are in the templates folder under views.
+        // the middle one is to render the view you want to see.
+        $this->load->view('templates/top');
+        $this->load->view("admin/publicar_anuncio");
+        $this->load->view('templates/footer');
+    }
+    
+    function login(){
+        //The first and third line imports the templates
+        // which are in the templates folder under views.
+        // the middle one is to render the view you want to see.
+        $this->load->view('templates/top');
+        $this->load->view("admin/login");
+        $this->load->view('templates/footer');
+        
+    }
+    function register(){
+        //The first and third line imports the templates
+        // which are in the templates folder under views.
+        // the middle one is to render the view you want to see.
+        $this->load->view('templates/top');
+        $this->load->view("admin/register");
+        $this->load->view('templates/footer');
+        
+    }
+    
 }
--- application/controllers/Start.php
+++ application/controllers/Start.php
@@ -1,38 +1,40 @@
 <?php
+date_default_timezone_set('America/Santo_Domingo');
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Start extends CI_Controller{
-
-  public function __construct()
-  {
-    parent::__construct();
-    //Codeigniter : Write Less Do More
-  }
-
-  function index()
-  {
-    //The first and third line imports the templates
-    // which are in the templates folder under views.
-    // the middle one is to render the view you want to see.
-    $this->load->view('templates/top');
-    $this->load->view('start');
-    $this->load->view('templates/footer');
-  }
-  function nosotros(){
-    //The first and third line imports the templates
-    // which are in the templates folder under views.
-    // the middle one is to render the view you want to see.
-    $this->load->view('templates/top');
-    $this->load->view('nosotros');
-    $this->load->view('templates/footer');
-  }
-function categoria(){
-  //The first and third line imports the templates
-  // which are in the templates folder under views.
-  // the middle one is to render the view you want to see.
-  $this->load->view('templates/top');
-  $this->load->view('categorias');
-  $this->load->view('templates/footer');
-}
-
+    
+    public function __construct()
+    {
+        parent::__construct();
+        //Codeigniter : Write Less Do More
+    }
+    
+    function index()
+    {
+        //The first and third line imports the templates
+        // which are in the templates folder under views.
+        // the middle one is to render the view you want to see.
+        $this->load->view('templates/top');
+        $this->load->view('start');
+        $this->load->view('templates/footer');
+    }
+    function nosotros(){
+        //The first and third line imports the templates
+        // which are in the templates folder under views.
+        // the middle one is to render the view you want to see.
+        $this->load->view('templates/top');
+        $this->load->view('nosotros');
+        $this->load->view('templates/footer');
+    }
+    function categoria(){
+        //The first and third line imports the templates
+        // which are in the templates folder under views.
+        // the middle one is to render the view you want to see.
+        $this->load->view('templates/top');
+        $this->load->view('categorias');
+        $this->load->view('templates/footer');
+    }
+    
 }
