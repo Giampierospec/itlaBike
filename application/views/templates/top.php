ea187fb3605530ef3d8c2ee051d2dd5d7bdb5e11
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-15 09:12:32 -0400

    changed all the files i've been hosting online ..
    to prevent network problems

--- application/views/templates/top.php
+++ application/views/templates/top.php
@@ -4,16 +4,16 @@
     <meta charset="utf-8">
 
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url('') ?>bootstrap/css/bootstrap.min.css">
 <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaina|Ranga" rel="stylesheet">
 <!-- jquery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="<?php echo base_url() ?>js/jquery.min.js"></script>
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="<?php echo base_url('') ?>bootstrap/css/bootstrap-theme.min.css">
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url('') ?>js/bootstrap.min.js"></script>
 <!-- Font Awesome -->
-<script src="https://use.fontawesome.com/01d9e54a7d.js"></script>
+<link rel="stylesheet" href="<?php echo base_url('') ?>font-awesome/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?php echo base_url('')?>css/main.css" />
 <link rel="stylesheet" href="<?php echo base_url('')?>css/login.css" />
     <title> Itla Bike | Proyecto Final</title>
