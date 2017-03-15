7cbda5173e78838c30f09afe5a638cde628ca07d
Nestor De la Cruz <nestoredelacruz@gmail.com>
2017-03-15 00:31:01 -0300

    Now when a user registers via Facebook, that user is added to the database

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,4 +1,5 @@
 <?php
+
 date_default_timezone_set('America/Santo_Domingo');
 
 defined('BASEPATH') OR exit('No direct script access allowed');
@@ -38,6 +39,8 @@ class Admin extends CI_Controller{
         
     }
     function register(){
+        $this->load->database();
+        
         //The first and third line imports the templates
         // which are in the templates folder under views.
         // the middle one is to render the view you want to see.
@@ -47,6 +50,26 @@ class Admin extends CI_Controller{
         {
             // User logged in, get user details
             $user = $this->facebook->request('get', '/me?fields=id,name,last_name,gender,email, picture');
+            
+            $CI =& get_instance();
+            $f = new stdClass();
+            $f->nombre = $user['name'];
+            $f->correo = $user['email'];
+            
+            // Check if the user exist in the database, if it does not exist add it
+            $sql = "select * from usuario where correo = ?";
+            $CI =& get_instance();
+            $email = $user['email'];
+            $rs = $CI->db->query($sql, array($email));
+            $rs = $rs->result();
+            
+            // The user exists in the database
+            if(count($rs) > 0){
+                // The user does not exist in the database
+            } else {
+                $CI->db->insert('usuario',$f);
+                
+            }
             
             if (!isset($user['error']))
             {
