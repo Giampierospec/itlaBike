5df734bd569907946d9a7060dccb83b7a41f729b
Nestor De la Cruz <nestoredelacruz@gmail.com>
2017-03-23 02:11:18 -0300

    Added the validation of not allow the login of a blocked user

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -5,23 +5,37 @@ $message = "";
 
 if($_POST){
     $sql = "select * from usuario where correo = ? and clave = ?";
+    $sql2 = "select * from usuario where correo = ? and bloqueado = 1";
+    
     $CI =& get_instance();
     $email = $_POST['email'];
     $password = md5($_POST['pass']);
     $rs = $CI->db->query($sql, array($email, $password));
+    $rs2 = $CI->db->query($sql2, array($email));
+    
     $rs = $rs->result();
-
+    $rs2 = $rs2->result();
+    
     // The user exists in the database
+    
     if(count($rs) > 0){
-        $_SESSION['itla_bike_user'] = $rs[0];
-        //I redirect this to admin because if it is set it is better to show the start page
-        // in this view you should be able to see all the info of the user
-        redirect('admin');
-        // The user does not exist in the database
+        
+        // The user is blocked in the database
+        if(count($rs2) > 0){
+            $message = "Usuario bloqueado";
+            
+        } else {
+            $_SESSION['itla_bike_user'] = $rs[0];
+            //I redirect this to admin because if it is set it is better to show the start page
+            // in this view you should be able to see all the info of the user
+            redirect('admin');
+            // The user does not exist in the database
+        }
     } else {
         $message = "Usuario o contraseña no válida";
     }
 }
+
 ?>
 
   <!-- This view is for logging in -->
