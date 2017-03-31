4e89c332ff551c55027bd97968622aba259cab27
Maxi-Heartnet <misael.the.mask@gmail.com>
2017-03-31 12:01:26 -0400

    "Share" integration changes discarded into categories
    At the end there's no need to add this functionality to this section

--- application/views/categorias.php
+++ application/views/categorias.php
@@ -1,16 +1,6 @@
 <?php
 $categorias = getAllCategorias();
 ?>
-<!--SDK de facebook para el boton compartir-->
-<div id="fb-root"></div>
-		<script>(function(d, s, id) {
-  			var js, fjs = d.getElementsByTagName(s)[0];
-  			if (d.getElementById(id)) return;
-  			js = d.createElement(s); js.id = id;
-  			js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.8&appId=1823829894500693";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));</script>
-
 <div id="catRow" class="row">
    
 </div>
@@ -37,13 +27,9 @@ $categorias = getAllCategorias();
                 <div class='panel-footer'>\
                 <p>"+cate[i].descripcion+"</p>\
                 </div>\
-
                 </div></a>\
                 </div>").fadeIn(2000);
                 
-			 //integracion del Boton compartir en el panel-footer de cada publicacion fuera de la seccion ver anuncio
-          <div class="fb-share-button" data-href="http://localhost:8080/itlaBike/application/views/templates/footer.php" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A8080%2FitlaBike%2Fapplication%2Fviews%2Ftemplates%2Ffooter.php&amp;src=sdkpreparse">Compartir</a></div>
-          
             
          }
      }
